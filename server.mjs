3cca7e0340eba7bc023aa7d4a31b8426f4a0f494
Singh2655 <singhshaurya222002@gmail.com>
2024-03-15 15:37:54 +0530

    connection upgrade to websocket

--- server.mjs
+++ server.mjs
@@ -1,17 +1,41 @@
 import {createServer} from "http"
+import crypto from "crypto"
 
 const PORT=8080
+const WEBSOCKET_MAGIC_STRING_KEY='258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
 
 const server=createServer((req,res)=>{
     res.writeHead(200)
-    // throw new Error('test')
     res.end("Hey there")
 })
 .listen(PORT,()=>console.log(`server listening on port ${PORT}`))
 
-server.on('upgrade',(req,socket,head)=>{
-    console.log({head:req.headers})
-})
+server.on('upgrade',onSocketUpgrade)
+
+function onSocketUpgrade(req,socket,head){
+    const {'sec-websocket-key':webSocketClientKey}=req.headers
+    console.log(`${webSocketClientKey} connected`)
+    const headers=prepareHandshakeHeader(webSocketClientKey)
+    socket.write(headers)
+}
+
+function prepareHandshakeHeader(id){
+    const acceptKey=createSocketAccept(id)
+    const headers=[
+        'HTTP/1.1 101 Switching Protocols',
+        'Upgrade: websocket',
+        'Connection: Upgrade',
+        `Sec-WebSocket-Accept: ${acceptKey}`,
+        ''
+    ].map(line=>line.concat('\r\n')).join('')
+    return headers
+}
+
+function createSocketAccept(id){
+    const shaum=crypto.createHash('sha1')
+    shaum.update(id+WEBSOCKET_MAGIC_STRING_KEY)
+    return shaum.digest('base64')
+}
 
 ;
 [
